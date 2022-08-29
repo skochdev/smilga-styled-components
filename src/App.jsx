9edf0c842eeead14e8c1f94bb821c7aabc7e6150
Bohdan Skochii <b.skochii@gmail.com>
2022-08-30 01:14:48 +0300

    add eslintignore

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box } from 'components/Box';
+import { GlobalStyle } from './GlobalStyle';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Box as="main" bg="white"></Box>
+    </>
+  );
+}
+
+export default App;
